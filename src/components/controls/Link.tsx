3f31d7274a0dc9004dd55188a5a676ea3ee18fca
Evans Owino <e.owino@reply.com>
2018-11-28 15:13:05 +0000

    Link styles

--- src/components/controls/Link.tsx
+++ src/components/controls/Link.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
+import { css as cssUtil } from '../../misc/';
 
 interface ILink extends React.ButtonHTMLAttributes<HTMLAnchorElement> {
-  href?: string;
-  btnLink?: boolean;
+  subtleLink?: boolean;
+  smallLink?: boolean;
   children: React.ReactChild;
 }
 
 const StyledLink = styled('a')({
   textTransform: 'uppercase',
+  textDecoration: 'none',
 });
 
-export default (props: ILink) => <StyledLink {...props} />;
+const subtleLink = css({
+  color: '#b3b3b3',
+  transitionDuration: '.2s',
+  '&:hover, &:focus': {
+    color: '#fff',
+    textDecoration: 'none',
+    borderBottomColor: 'currentcolor',
+  },
+});
+
+const smallLink = css({
+  fontSize: '12px',
+  color: '#b3b3b3',
+  borderBottom: '1px solid rgba(0,0,0,0)',
+  paddingBottom: 3,
+  textTransform: 'none',
+  '&:hover, &:focus': {
+    color: '#fff',
+    borderBottomColor: 'currentcolor',
+  },
+});
+
+export const Link = ({ className = '', ...props }: ILink) => (
+  <StyledLink
+    {...props}
+    className={cssUtil(
+      className,
+      props.subtleLink ? subtleLink : '',
+      props.smallLink ? smallLink : ''
+    )}
+  />
+);
