ac5e2758c2b117eba3e7ad177a8f53eb80461da1
Evans Owino <evansowino@hotmail.com>
2018-12-05 10:30:49 +0000

    adding slider ball to progress bar

--- src/components/controls/ProgressBar.tsx
+++ src/components/controls/ProgressBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
+import { css as cssUtil } from '../../misc';
 import { spotifyCalm, spotifyGrey, spotifyGreen } from '../../styles';
 
 interface IProgressBar {
-  cssWidth?: string;
+  cssWidth?: number;
 }
 
 const StyledProgressBarWrapper = styled('div')({
@@ -20,6 +21,28 @@ const sharedStyles = css({
 
 const StyledBackground = styled('div')({
   backgroundColor: spotifyGrey,
+  '&:hover': {
+    '.slider-fg': {
+      backgroundColor: spotifyGreen,
+      cursor: 'pointer',
+    },
+    '.slider-ball': {
+      display: 'block',
+    },
+  },
+});
+
+const StyledSliderBall = styled('span')({
+  backgroundColor: spotifyCalm,
+  borderRadius: '100%',
+  display: 'none',
+  height: 14,
+  width: 14,
+  position: 'absolute',
+  bottom: 3,
+  '&:hover': {
+    cursor: 'pointer',
+  },
 });
 
 const StyledForeground = styled('div')({
@@ -27,20 +50,25 @@ const StyledForeground = styled('div')({
   transitionDuration: '.1s',
   transitionTimingFunction: 'cubic-bezier(1,0,.7,1)',
   transitionProperty: 'background-color',
-  '&:hover': {
-    backgroundColor: spotifyGreen,
-  },
 });
 
 export class ProgressBar extends React.PureComponent<IProgressBar> {
+  width = (isBall?: boolean) => {
+    const cssWidth = this.props.cssWidth || 0;
+    return isBall ? `${cssWidth - 1}%` : `${cssWidth}%`;
+  };
+
   render() {
-    const { cssWidth } = this.props;
     return (
       <StyledProgressBarWrapper>
         <StyledBackground className={sharedStyles}>
           <StyledForeground
-            className={sharedStyles}
-            style={{ width: cssWidth || '100%' }}
+            className={cssUtil('slider-fg', sharedStyles)}
+            style={{ width: this.width() }}
+          />
+          <StyledSliderBall
+            className="slider-ball"
+            style={{ left: this.width(true) }}
           />
         </StyledBackground>
       </StyledProgressBarWrapper>
